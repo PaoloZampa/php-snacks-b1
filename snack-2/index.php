a043784028ec738a5e899d67a514c879d435253c
PaoloZampa <nlmp.paolozampa@gmail.com>
2023-05-04 12:59:05 +0200

    commit

---
+++ snack-2/index.php
@@ -0,0 +1,67 @@
+<?php
+
+/* ## Snack 2
+Con un form passare come parametri GET name, mail e age e verificare (cercando i metodi che non conosciamo nella documentazione) che name sia più lungo di 3 caratteri, che mail contenga un punto e una chiocciola e che age sia un numero. Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato” */
+
+?>
+
+<!doctype html>
+<html lang="en">
+
+<head>
+    <title>Snack 2</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS v5.2.1 -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+
+</head>
+
+<body>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-6 card shadow">
+                <div class="card-body">
+                    <form action="verify.php" method="get">
+                        <div class="mb-3">
+                            <label for="name">Name</label>
+                            <input type="text" name="name" id="name" class="form-control" placeholder="name"
+                                aria-describedby="suffixId">
+                        </div>
+                        <div class="mb-3">
+                            <label for="age">Age</label>
+                            <input type="text" name="age" id="age" class="form-control" placeholder="age"
+                                aria-describedby="suffixId">
+                        </div>
+                        <div class="mb-3">
+                            <label for="mail">E-mail</label>
+                            <input type="text" name="mail" id="mail" class="form-control" placeholder="email"
+                                aria-describedby="suffixId">
+                        </div>
+
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"
+        integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous">
+        </script>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.min.js"
+        integrity="sha384-7VPbUDkoPSGFnVtYi0QogXtr74QeVeeIs99Qfg5YCF+TidwNdjvaKZX19NZ/e6oz" crossorigin="anonymous">
+        </script>
+</body>
+
+</html>
